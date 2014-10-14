78c6789767d2f8442079c60c5fd54054aa50f7ef
dervalp <dervalp@gmail.com>
2014-10-14 15:05:08 +0100

    add lowerFirstLetter

--- src/utils/string.js
+++ src/utils/string.js
@@ -1,14 +1,18 @@
 var idCounter = 0;
 
-var capitalize = function( str ) {
-        return str.charAt( 0 ).toUpperCase() + str.slice( 1 );
+var capitalize = function(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
     },
-    uniqueId = function( prefix ) {
+    lowerFirstLetter = function(str) {
+        return str.charAt(0).toLowerCase() + str.slice(1);
+    },
+    uniqueId = function(prefix) {
         var id = ++idCounter + "";
         return prefix ? prefix + id : id;
     };
 
 module.exports = {
     capitalize: capitalize,
+    lowerFirstLetter: lowerFirstLetter,
     uniqueId: uniqueId
 };
